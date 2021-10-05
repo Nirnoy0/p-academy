659c9dbb84289f2046a12c09be8cf7f748e1cc91
Muhayminul Nirnoy <86642381+Nirnoy0@users.noreply.github.com>
2021-10-05 20:52:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import About from './components/About/About';
+import Home from './components/Home/Home';
 
 function App() {
+  useEffect( () => {
+    fetch('./courses.JSON')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <About></About>
+      <Home></Home>
     </div>
   );
 }
